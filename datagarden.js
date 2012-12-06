9b009dbbc9dbdd5f3b1a758b107bebe681684655
juleea <julia@cs.stanford.edu>
2012-12-05 19:58:39 -0800

    Fix bug in rickshaw.js hover. Add datagarden and 10s data intervals

--- datagarden.js
+++ datagarden.js
@@ -2,30 +2,40 @@
 function DataGarden(garden, useLive) { 
 	this.garden = garden;
 	this.useLive = useLive;
-	
+
+	var that = this;
 
 	if (!useLive) {
-		this.now = new Date("2012-12-04T16:40:04.000Z");
+		this.now = new Date("2012-12-04T15:30:04.000Z");
 		this.bigBang = new Date("2012-12-04T15:25:14.000Z");
 		this.apocalypse = new Date("2012-12-04T19:18:24.000Z");
-		this.currentIndex = 0;
+		this.nextIndex = 0;
 
-		$.getJSON("../data/v2.n1440.clean", function(response) {
-			this.data = response;
-		})
+		$.getJSON("../data/v2.n1440.clean.json", function(response) {
+			that.data = response.reverse();
+			console.log("data retrieved: " + that.data.length + " points");
+		});
+
 	}
-
 }
 
 DataGarden.prototype.getLastN = function(n) {
-}
-
-DataGarden.prototype.getIntialData = function() {
+	console.log("getLastN()");
 	if (!this.useLive) {
-		var then = this.bigBang;
-		while (this.now > then) {
-			this.currentIndex++;
-		}
-	return this.data.slice(0, this.currentIndex);
+		var start = this.nextIndex;
+		this.nextIndex = start + n;
+		console.log(this.data[0].time + " ... " + this.data[1].time);
+		return this.data.slice(start, this.nextIndex);
 	}
 }
+
+DataGarden.prototype.getInitialData = function() {
+	console.log("get initial data()");
+	if (!this.useLive) {
+		console.log(this.now.getTime() - this.bigBang.getTime());
+		var numPoints = (this.now.getTime() - this.bigBang.getTime())/10.0/1000;
+		this.nextIndex = numPoints;
+		return this.data.slice(0, this.nextIndex);
+	}
+}
+

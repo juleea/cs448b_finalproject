257ec77a4016cece6e2b3578ed591e8ea33c9257
JJ <jie.squared@gmail.com>
2012-12-10 21:14:47 -0800

    trigger on done, fix datetimepicker z-index

--- rickshaw/js/jquery-ui-timepicker-addon.js
+++ rickshaw/js/jquery-ui-timepicker-addon.js
@@ -469,6 +469,8 @@
 				
 				// inject timepicker into datepicker
 				var $buttonPanel = $dp.find('.ui-datepicker-buttonpane');
+                $buttonPanel[0].firstChild.nextSibling.setAttribute("id", "doneButton");
+                console.log($buttonPanel[0].firstChild.nextSibling);
 				if ($buttonPanel.length) {
 					$buttonPanel.before($tp);
 				} else {

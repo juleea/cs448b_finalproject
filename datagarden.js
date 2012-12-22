fac11fbdafdf280f6f157a8d7e05605afddf3f02
juleea <julia@cs.stanford.edu>
2012-12-22 15:01:20 -0800

    Update datagarden and dashboardlive

--- datagarden.js
+++ datagarden.js
@@ -2,9 +2,10 @@ function DataGarden(garden, useLive) {
 
 	this.garden = garden;
 	this.useLive = useLive;
-
+	
 	var that = this;
 
+	//use local static data
 	if (!useLive) {
 		this.now = new Date("2012-12-05T08:32:56.000Z");
 		this.bigBang = new Date("2012-12-05T08:08:56.000Z");
@@ -15,11 +16,15 @@ function DataGarden(garden, useLive) {
 			that.data = response;
 		});
 
+	} else { //use live data!
+
 	}
 }
 
+DataGarden.prototype.DEFAULT_VISIBLE_TIME_RANGE = 5 * 60 * 1000; // 5 minutes
+
+
 DataGarden.prototype.getLastN = function(n) {
-	// console.log("getLastN()");
 	if (!this.useLive) {
 		// console.log(this.data.slice(0, 9));
 		var start = this.nextIndex;
@@ -27,22 +32,24 @@ DataGarden.prototype.getLastN = function(n) {
 		//console.log("n = " + n + "index range: [" + start + ", " + this.nextIndex + "]");
 		return this.data.slice(start, this.nextIndex);
 
+	} else {
+
 	}
 }
 
 
-DataGarden.prototype.getInitialData = function(interval) {
-/*DataGarden.prototype.getInitialData = function() {
-	if (!this.useLive) {
-		var numPoints = (this.now.getTime() - this.bigBang.getTime())/10.0/1000;
-		this.nextIndex = numPoints;
-		console.log("intial display num points: " + this.nextIndex);
-		return this.data.slice(0, this.nextIndex);
-        */
+DataGarden.prototype.getInitialData = function(start, interval) {
+	if (!start) {
+		start = new Date().getTime();
+	}
+
+	if (!interval) {
+		interval = DEFAULT_VISIBLE_TIME_RANGE;	
+	}
 
 	// console.log("get initial data()");
 	if (!this.useLive) { 
-		var start = this.bigBang.getTime();
+		start = this.bigBang.getTime();
 		if (interval) {
 			start = this.now.getTime() - interval;
 
@@ -52,10 +59,7 @@ DataGarden.prototype.getInitialData = function(interval) {
 		// console.log("intial display num points: " + this.nextIndex);
 		return returnData;
 		// return this.data.slice(0, this.nextIndex);
-	
-	} else if (now && interval) {
-		return this.getRange(now - interval, now); 
-	
+		
 	} else {
 		//TODO: change return condition?
 		console.error("Error retrieving initial data. Returning empty array.");
@@ -67,19 +71,34 @@ DataGarden.prototype.getInitialData = function(interval) {
 // start time is inclusive, end time is exclusive
 DataGarden.prototype.getRange = function(start, end) {
 
+	// var queryUrl = "http://50.19.108.27/arduino/garden/" + garden + "?num=" + numEntries;
+
+	if (end < start) {
+		console.error("Invalid query: end is before start. Returning empty array.");			
+		//TODO: change return condition?
+		return [];
+	}
+
 	if (!this.useLive) {
-		if (end < start) {
-			console.error("Invalid query: end is before start. Returning empty array.");
-			
-			//TODO: change return condition?
-			return [];
-		}
         $("#preloader").hide(); // this is probably awkwardly not in the right place :(
         $("#inputs").show(); // this is probably awkwardly not in the right place :(
-        //console.log(this.bigBang.getTime());
 		var startIndex = Math.min(Math.max(Math.floor((start - this.bigBang.getTime()) / this.dataInterval), 0), this.data.length);
 		var endIndex = Math.max(Math.min(Math.floor((end - this.bigBang.getTime()) / this.dataInterval), this.data.length), 0);
 		console.log("Effective index range queried: [" + startIndex  + ", " + endIndex + "]");
 		return this.data.slice(startIndex, endIndex);
+
+	} else {
+		http://50.19.108.27/arduino/garden/range/v2?start_time=2012-11-20T02:37:38.963Z&end_time=2012-11-28T02:37:38.963Z&num=10
+		var queryUrl = "http://50.19.108.27/arduino/garden/range/" + garden + "?end_time=" + curTime + "&num=" + numEntries;
+	    
+	    var results = [];
+	    jQuery.ajax({
+	        url: queryUrl,
+	        type: "GET",
+	        dataType: "json",
+	  		success: function(data, textStatus, jqXHR) { results = data; console.log("range retrieved.") }
+	    });
+	    console.log( "returning results" );
+	    return results;
 	}
 }
